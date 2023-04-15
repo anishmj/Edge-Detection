9b9766f060fe2484f31d8b05da7017383fea4680
anishmj <mjanish19@gmail.com>
2023-04-15 10:44:59 +0530

    ASD

--- dip exp7.py
+++ dip exp7.py
@@ -1,52 +1,43 @@
-# Edge-Detection
-## Aim:
-To perform edge detection using Sobel, Laplacian, and Canny edge detectors.
+#!/usr/bin/env python
+# coding: utf-8
 
-## Software Required:
-Anaconda - Python 3.7
+# In[2]:
 
-## Algorithm
-
-### Step1:
-Import the required packages.
-
-### Step2:
-Convert the input image to gray , to get more details and for laplcian operator, we have to convert input image to bgr format
-
-### Step3:
-Apply smoothing to reduce noise, here we are using gaussian blur
-
-### Step4:
-Perform the edge detector operation
-
-### Step5:
-Show the detected image
- 
-## Program:
-~~~
-NAME : ANISH MJ
-REG NO : 212221230005
-~~~
-
-~~~
-# Import the packages
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
-
-# Load the image, Convert to grayscale and remove noise
-
 img = cv2.imread('city.jpeg')
 cv2.imshow("original",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+# In[ ]:
+
+
+img = cv2.imread('city.jpeg')
+scale = 10000 
+width = int(img.shape[1] * scale / 100)  
+height = int(img.shape[0] * scale / 100) 
+dim = (width, height)  
+# resize image  
+resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA) 
+plt.imshow(resized)
+plt.show()
+
+
+# In[3]:
+
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray",gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# In[4]:
+
 
 img = cv2.GaussianBlur(gray,(3,3),0)
 cv2.imshow("Gaussian blur",img)
@@ -54,9 +45,16 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
+# In[5]:
 
 
-# SOBEL EDGE DETECTOR
+gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("bgr2gray",gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[6]:
 
 
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
@@ -66,6 +64,10 @@ plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
 cv2.imshow("SobelX",sobelx)
 cv2.waitKey(0)
 cv2.destroAllWindows()
+
+
+# In[7]:
+
 
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
 plt.figure(1)
@@ -77,13 +79,18 @@ cv2.imshow("sobely",sobely)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
+
+# In[8]:
+
+
 sobel_xy=cv2.Sobel(img,cv2.CV_64F,1,1,ksize=5)
 cv2.imshow('sobel_xy',sobel_xy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# LAPLACIAN EDGE DETECTOR
+# In[9]:
+
 
 # LAPLACIAN EDGE DETECTOR AND TO SHOW THE DETECTED IMAGE
 
@@ -93,7 +100,9 @@ cv2.imshow('laplacian_operator',laplacian_operator)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# CANNY EDGE DETECTOR
+
+# In[10]:
+
 
 canny_edges = cv2.Canny(img,120,150)
 plt.figure(1)
@@ -101,23 +110,9 @@ cv2.imshow("Canny Edges",canny_edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
-~~~
 
-## Output:
-### ORIGINAL IMAGE
-![a](ori.png)
-### SOBEL EDGE DETECTOR
-![a](sobelx.png)
-![a](sobely.png)
-![a](sobelxy.png)
+# In[ ]:
 
 
-### LAPLACIAN EDGE DETECTOR
-![p](laplacian.png)
 
 
-### CANNY EDGE DETECTOR
-![a](canny.png)
-
-## Result:
-Thus the edges are detected using Sobel, Laplacian, and Canny edge detectors.
